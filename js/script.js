fad6697fab34dea5808ad6025c7e63cd44345ff6
Daniel-0117 <nevaslon0117@gmail.com>
2024-10-02 07:23:26 -0700

    started Javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-const weatherKey = {apiKey: "b5f7e35f7037d22bd6e598b6924f2fc0"};
+const weatherKey = `b5f7e35f7037d22bd6e598b6924f2fc0`;
 const weatherUrl = "https://api.openweathermap.org";
 const weatherHistory = [];
+
+// Grabs DOM Elements
+
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const weatherDisplay = document.getElementById("weather-display");
+const weatherHistoryDisplay = document.getElementById("weather-history-display");
 
 
