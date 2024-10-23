b04765f6664a3da68684c3c76e25e18c956a32e1
Daniel-0117 <nevaslon0117@gmail.com>
2024-10-23 16:40:57 -0700

    added basic Java Elements

--- js/script.js
+++ js/script.js
@@ -9,4 +9,58 @@ const searchButton = document.getElementById("search-button");
 const weatherDisplay = document.getElementById("weather-display");
 const weatherHistoryDisplay = document.getElementById("weather-history-display");
 
+// Event Listeners
+
+//Inserts boxs with the weather data
+searchButton.addEventListener("click", function () {
+  const searchValue = searchInput.value;
+  getWeather(searchValue);
+});
+
+//Function to fetch and display the weather data
+async function getWeather(city) {
+  try {
+    const response = await fetch(`${weatherUrl}/data/2.5/weather?q=${city}&appid=${weatherKey}&units=imperial`);
+    if (response.ok) {
+      const data = await response.json();
+      renderWeather(data); // pass the waeather data to the renderWeather function
+      weatherHistory.push(data); // push the data to the weatherHistory array
+      renderWeatherHistory(); // call the renderWeatherHistory function
+
+    } else {
+      alert("City not found");
+    }
+  } catch (error) {
+    console.error("Error catching weather data", error);
+  }
+}
+
+//Function to render the weather data
+function renderWeather(weatherData) {
+  const weatherBox = document.createElement("div");
+  weatherBox.classList.add("weather-box");
+  weatherBox.innerHTML = `
+    <h2>${weatherData.name}</h2>
+    <p>Temperature: ${weatherData.main.temp}°F</p>
+    <p>Humidity: ${weatherData.main.humidity}%</p>
+    <p>Wind Speed: ${weatherData.wind.speed}mph</p>
+  `;
+  weatherDisplay.appendChild(weatherBox);
+}
+
+//Function to render the weather history
+function renderWeatherHistory() {
+  weatherHistoryDisplay.innerHTML = "";
+  weatherHistory.forEach((weatherData) => {
+    const weatherBox = document.createElement("div");
+    weatherBox.classList.add("weather-box");
+    weatherBox.innerHTML = `
+      <h2>${weatherData.name}</h2>
+      <p>Temperature: ${weatherData.main.temp}°F</p>
+      <p>Humidity: ${weatherData.main.humidity}%</p>
+      <p>Wind Speed: ${weatherData.wind.speed}mph</p>
+    `;
+    weatherHistoryDisplay.appendChild(weatherBox);
+  });
+}
 
